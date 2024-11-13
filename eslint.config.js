c9455ab18ccecafd10339628d5dbb6d8fb4ed7a8
ashuicoder <1249735120@qq.com>
2024-11-13 10:07:59 +0800

    ✨ feat: eslint配置完善

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,22 @@
+import path from 'node:path'
 import pluginVue from 'eslint-plugin-vue'
 import vueTsEslintConfig from '@vue/eslint-config-typescript'
 import pluginVitest from '@vitest/eslint-plugin'
 import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
+import { includeIgnoreFile } from '@eslint/compat'
+import { fileURLToPath } from 'node:url'
 
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const gitignorePath = path.resolve(__dirname, '.gitignore')
+const prettierignorePath = path.resolve(__dirname, '.prettierignore')
 export default [
   {
     name: 'app/files-to-lint',
     files: ['**/*.{ts,mts,tsx,vue}'],
   },
-
+  includeIgnoreFile(gitignorePath),
+  includeIgnoreFile(prettierignorePath),
   {
     name: 'app/files-to-ignore',
     ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
@@ -22,4 +30,11 @@ export default [
     files: ['src/**/__tests__/*'],
   },
   skipFormatting,
+
+  {
+    name: 'custom-rules',
+    rules: {
+      'no-undef': 'error',
+    },
+  },
 ]
